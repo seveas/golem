0c2b176edf0f3d7fb23477d86c61a8f6bbdd97c7
Dennis Kaarsemaker <dennis@kaarsemaker.net>
2014-01-05 23:07:10 +0100

    Allow for multiple source packages in the debian_source worker

--- golem/worker/debian_source_split.py
+++ golem/worker/debian_source_split.py
@@ -7,24 +7,26 @@ class Daemon(Worker):
     repo_checkout = False
 
     def process_job_simple(self, job):
-        self.logger.info("Building debian source packages")
-        # Fetch tarball created in previous task
+        self.logger.info("Splitting debian source packages")
+        # Fetch package created in previous task
         orig_files = job.fetch_artefacts(job.requires[0], "*")
-        dsc = [x for x in orig_files if x.endswith('.dsc')][0]
 
-        # Unpack sources, and build extra packages
-        job.shell.dpkg_source('-x', dsc)
-        pkgdir = os.path.abspath(glob.glob("*/")[0]) # Ugly much?
-        with open(os.path.join(pkgdir, 'debian', 'changelog')) as fd:
-            line = fd.readline()
-            version = re.search(r'(?<=\().*(?=\))', line).group(0)
-        for release in job.release:
-            self.logger.info("Building source package for release %s" % release)
-            job.shell.sed('-e', '1s/(.*).*;/(%s~%s) %s;/' % (version, release, release), '-i', 'debian/changelog', cwd=pkgdir)
-            args = getattr(job, 'debuild_args', ['-S', '-si'])
-            if isinstance(args, basestring):
-                args = [args]
-            job.shell.debuild(*args, cwd=pkgdir)
+        for dsc in orig_files:
+            if not dsc.endswith('.dsc'):
+                continue
+            # Unpack sources, and build extra packages
+            job.shell.dpkg_source('-x', dsc)
+            pkgdir = os.path.abspath(glob.glob("*/")[0]) # Ugly much?
+            with open(os.path.join(pkgdir, 'debian', 'changelog')) as fd:
+                line = fd.readline()
+                version = re.search(r'(?<=\().*(?=\))', line).group(0)
+            for release in job.release:
+                self.logger.info("Building source package for release %s" % release)
+                job.shell.sed('-e', '1s/(.*).*;/(%s~%s) %s;/' % (version, release, release), '-i', 'debian/changelog', cwd=pkgdir)
+                args = getattr(job, 'debuild_args', ['-S', '-si'])
+                if isinstance(args, basestring):
+                    args = [args]
+                job.shell.debuild(*args, cwd=pkgdir)
 
         for changes in glob.glob('*.changes'):
             if changes in orig_files:
