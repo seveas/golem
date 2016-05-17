4f8f0f58ef17775a3cd4dd2b80127401598715f5
Dennis Kaarsemaker <dennis@kaarsemaker.net>
2016-05-17 20:39:15 +0200

    debian worker: better release config

--- golem/worker/debian_source_split.py
+++ golem/worker/debian_source_split.py
@@ -1,11 +1,19 @@
 from golem.worker import Worker, GolemError
+import csv
 import glob
 import os
 import re
+import requests
 import shutil
+import stat
+import time
 
 class Daemon(Worker):
     repo_checkout = False
+    distro_info = {
+        'debian': 'http://anonscm.debian.org/cgit/collab-maint/distro-info-data.git/plain/debian.csv',
+        'ubuntu': 'http://anonscm.debian.org/cgit/collab-maint/distro-info-data.git/plain/ubuntu.csv',
+    }
 
     def process_job_simple(self, job):
         self.logger.info("Splitting debian source packages")
@@ -21,7 +29,7 @@ class Daemon(Worker):
             with open(os.path.join(pkgdir, 'debian', 'changelog')) as fd:
                 line = fd.readline()
                 version = re.search(r'(?<=\().*(?=\))', line).group(0)
-            for release in job.release:
+            for release in self.expand_releases(job.release):
                 self.logger.info("Building source package for release %s" % release)
                 job.shell.sed('-e', '1s/(.*).*;/(%s~%s) %s;/' % (version, release, release), '-i', 'debian/changelog', cwd=pkgdir)
                 args = getattr(job, 'debuild_args', ['-S', '-si'])
@@ -36,3 +44,36 @@ class Daemon(Worker):
             for src in job.shell.dcmd(changes).stdout.strip().split():
                 if os.path.exists(src): # Can't move the orig.tar.gz more than once
                     os.rename(src, os.path.join(job.artefact_path, os.path.basename(src)))
+
+    def expand_releases(self, releases):
+        if isinstance(releases, basestring):
+            releases = [releases]
+        cache = os.path.join(os.path.expanduser('~'), '.cache', 'distro-info-data')
+        if not os.path.exists(cache):
+            os.makedirs(cache)
+        now = time.time()
+        today = time.strftime('%Y-%m-%d')
+        distinfo = {}
+        for name,url in self.distro_info.items():
+            path = os.path.join(cache, name)
+            if not os.path.exists(path) or os.stat(path)[stat.ST_MTIME] < now - 604800:
+                with open(path,'w') as fd:
+                    fd.write(requests.get(url).text)
+            with open(path) as fd:
+                distinfo[name] = list(csv.DictReader(fd))
+
+        for release in releases:
+            if not release.endswith('+'):
+                yield release
+                continue
+            release = release[:-1]
+            for dist in self.distro_info:
+                data = distinfo[dist]
+                if release in [r['series'] for r in data]:
+                    oldest = [r['created'] for r in data if r['series'] == release][0]
+                    for release in data:
+                        if (release['created'] >= oldest and                    # Filter out older releases
+                            release['created'] < today and                      # Debian lists upcoming not-yet-created releases
+                            (release['eol'] > today or not release['eol']) and  # active Debian releases may not have an eol set
+                            release['version']):                                # experimental/sid don't have versions
+                            yield release['series']
